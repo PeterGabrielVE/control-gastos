ef7c9d92c64c25e348a268f9bbf41b371a13ed33
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-05-22 23:04:14 -0400

    creando formulario

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra">
         <form className='formulario'>
             <div className='campo'>
                 <label htmlFor="">Definir Presupuesto</label>
                 <input className="nuevo-presupuesto" type="text"
-                placeholder='Añade tu presupuesto' />
+                placeholder='Añade tu presupuesto'
+                value={ presupuesto }
+                onChange={ e => setPresupuesto(e.target.value)}
+                />
             </div>
             <input type="submit" value="Añadir" />
         </form>
