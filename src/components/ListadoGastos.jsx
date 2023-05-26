a9a3c57b17d2720c73026b2e8a41e46804d40818
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-05-25 21:55:54 -0400

    gasto

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -6,7 +6,10 @@ const ListadoGastos = ({ gastos }) => {
   return (
     <div className="listado-gastos contenedor">
       <h2>{ gastos.length ? 'Gastos' : 'No hay gastos aún' }</h2>
-      {gastos.map( gasto => ( <Gasto /> ) )}
+      {gastos.map( gasto => ( 
+      <Gasto
+       key={ gasto.id } 
+       gasto={ gasto } /> ) )}
     </div>
   )
 }
