67c9363a6d1c2e235be67cfc39e7878b0c93c9e1
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-06-14 20:23:19 -0400

    eliminar gastos

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,11 @@ function App() {
     }, 500);
   }
 
+  const eliminarGasto = id =>{
+    const gastosActualizados = gastos.filter( gasto => gasto.id !== id );
+    setGastos(gastosActualizados)
+  }
+
   const guardarGasto = gasto =>{
 
     if(gasto.id){
@@ -70,6 +75,7 @@ function App() {
             <ListadoGastos
             gastos={ gastos }
             setGastoEditar={ setGastoEditar }
+            eliminarGasto={ eliminarGasto }
             />
           </main>
           <div className="nuevo-gasto">
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -27,7 +27,7 @@ const diccionarioIconos = {
   suscripciones : IconoSuscripciones
 }
 
-const Gasto = ({ gasto, setGastoEditar }) => {
+const Gasto = ({ gasto, setGastoEditar, eliminarGasto }) => {
 
   const { categoria, nombre, cantidad, id, fecha } = gasto;
 
@@ -42,10 +42,9 @@ const Gasto = ({ gasto, setGastoEditar }) => {
   const trailingActions = () => (
     <TrailingActions>
         <SwipeAction 
-            onClick={ () => console.log('eliminars...')}
-           
-        >
-            Eliminar
+            onClick={ () => eliminarGasto(id)}
+            destructive={true}
+        >Eliminar
         </SwipeAction>
     </TrailingActions>
 )
